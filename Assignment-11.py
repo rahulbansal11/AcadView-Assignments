5e2563ad1dad160900ed47ea0ce10ecd368e27e3
rahulbansal11 <41982607+rahulbansal11@users.noreply.github.com>
2018-08-30 11:36:57 +0530

    Create Assignment-11.py

---
+++ Assignment-11.py
@@ -0,0 +1,7 @@
+1>>Write a python code to find a valid email address from a text:-
+
+[a-zA-Z0-9_.]*[@](gmail.com|yahoo.com|icloud.com|)
+
+2>>Write a python program to find a valid Indian phone number from a text.(Valid Indian numbers will start with "+91-" and after that [6-9] followed by 9 digits.):-
+
+^(+91-)[6-9]\d{9}
